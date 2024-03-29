80e2479108be7b1e086c708485e26c273d94d54a
sanziana <sanzianagrecu@gmail.com>
2024-03-29 20:12:39 +0200

    start project, initialize tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-yellow': 'hsl(47, 88%, 63%)',
+        white: 'hsl(0, 0%, 100%)',
+        grey: 'hsl(0, 0%, 50%)',
+        black: 'hsl(0, 0%, 7%)',
+      },
+      fontFamily: {
+        'figree': ["Figtree", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
